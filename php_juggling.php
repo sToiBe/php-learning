d0c11149cfa171e9893f760d62e81549a2e0b7a9
Dev StoiBe <dev.stoibe@gmail.com>
2023-02-12 01:54:08 -0300

    practice php juggling

--- php_juggling.php
+++ php_juggling.php
@@ -1,3 +1,40 @@
 <?php
 
 # PHP don't require explicit type definition in variable declaration
+
+# NOTE: when A VALUE needs to be interpreted as a different type
+# the VALUE ITSELF DOES NOT change type
+
+#NUMERIC CONTEXTS
+
+$foo = "0"; //string
+var_dump($foo);
+$foo = $foo + 2; // int
+var_dump($foo);
+$foo = $foo + 1.3; // float
+var_dump($foo);
+$foo = 5 + "10 cerditos pequeños";
+var_dump($foo); // $foo = int(15)
+$foo = 5 + "10 Cerdos pequeños";
+var_dump($foo); // $foo = int(15)
+
+
+#ARRAYS
+
+$a = 'CAR';
+$a[0] = 'B';
+echo $a;
+
+$foo = 10;
+$bar = (string) $foo;
+
+var_dump($bar);
+
+
+$foo = 100;
+$str = "$foo";
+$fst = (string) $foo;
+
+if ($fst === $str) {
+    echo "They are the same";
+}
